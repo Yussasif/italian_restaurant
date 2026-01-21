e1e197cba7e53990a88f1c2ac80367708c686dfe
yussasif <yusufowoade2@gmail.com>
2026-01-21 20:34:59 +0100

    first mobile resolution fix

--- src/components/Carousel.jsx
+++ src/components/Carousel.jsx
@@ -1,9 +1,8 @@
 import { useEffect, useState } from "react";
-import { dishes } from "../assets/data/data"
+import { dishes } from "../assets/data/data";
 import DishCard from "./DishCard";
 
 function Carousel() {
-
   const [current, setCurrent] = useState(0);
 
   const nextSlide = () => {
@@ -16,20 +15,20 @@ function Carousel() {
     return () => clearInterval(interval);
   }, [current]);
 
-
-    return (
-        <div className="w-full overflow-hidden relative">
-      <div className="flex transition-transform duration-500 ease-in-out" style={{ transform: `translateX(-${current * 100}%)` }}>
+  return (
+    <div className="w-full overflow-hidden relative">
+      <div
+        className="flex transition-transform duration-500 ease-in-out"
+        style={{ transform: `translateX(-${current * 100}%)` }}
+      >
         {dishes.map((dish, index) => (
           <div key={index} className="w-full shrink-0 flex justify-center">
             <DishCard dish={dish} />
           </div>
-
         ))}
       </div>
     </div>
-
-    )
+  );
 }
 
-export default Carousel
+export default Carousel;
